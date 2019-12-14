62e4f04d7d87e44c818823c149b53446a8fed153
Ильин А.Д <56357608+sashaspb09@users.noreply.github.com>
2019-12-14 12:49:37 +0300

    Update Person.java

--- src/ru/avalon/java/j20/labs/models/Person.java
+++ src/ru/avalon/java/j20/labs/models/Person.java
@@ -1,9 +1,11 @@
 package ru.avalon.java.j20.labs.models;
+import java.util.Objects;
 
 /**
  * Представление о человеке.
  */
 public class Person {
+
     /**
      * Имя.
      */
@@ -14,8 +16,7 @@ public class Person {
     private final String surname;
 
     /**
-     * Создаёт экземпляр класса на основании имени и даты
-     * рождения.
+     * Создаёт экземпляр класса на основании имени и даты рождения.
      *
      * @param name имя человека
      * @param surname дата рождения человека
@@ -23,6 +24,40 @@ public class Person {
     public Person(String name, String surname) {
         this.name = name;
         this.surname = surname;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 59 * hash + Objects.hashCode(this.name);
+        hash = 59 * hash + Objects.hashCode(this.surname);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Person other = (Person) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.surname, other.surname)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + surname;
     }
 
     /**
